cb9faa737472577c034bee77910ad5c67519437f
Hieu Dang <dangh2809@gmail.com>
2022-05-07 18:49:14 -0400

    finish

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import {React, useEffect }from 'react';
 import { Text, View } from 'react-native';
+import { initializeApp } from 'firebase/app';
+import {getFirestore, collection, addDoc, Timestamp} from "firebase/firestore";
+import {addPost, addReplyToPost} from "./backend/apis"
+import { Alert } from "react-native";
+import db from './firebaseConfig.js';
 
-const YourApp = () => {
+const App = () => {
+
+  async function addPost(question, time){
+    try{
+      await addPost(question, time)
+    } catch(e){
+      Alert.alert("Cannot post. Error occur");
+    }
+  }
+
+
+
+  // try {
+  //   const res = await addDoc(collection(db, "posts"), {
+  //     question: "Is everyone good at learning CS?"
+  //   });
+  //   console.log(res.id)
+  // }
+  // catch(e){
+  //   console.error("Error adding post: ", e)
+  // }
+  
+  
+  //   console.log('Added document with ID: ', res.id);
+  
+   
+
+  
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>
-        Hello  🎉
+        Hello  ada
       </Text>
     </View>
   );
 }
 
-export default YourApp;
+export default App;
