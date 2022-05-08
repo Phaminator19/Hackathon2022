dbee47c31e5e7066eb3f59830c197a5762ebeb27
Hieu Dang <dangh2809@gmail.com>
2022-05-07 23:45:28 -0400

    add posts to page

--- App.js
+++ App.js
@@ -7,14 +7,17 @@ import Feed from './components/Feed';
 import {getFirestore, collection, addDoc, Timestamp} from "firebase/firestore";
 import {addPost, addReplyToPost, getAllRecords} from "./backend/apis"
 import { Alert } from "react-native";
+import TimeAgo from 'javascript-time-ago';
 
+import en from 'javascript-time-ago/locale/en.json'
 
-
+TimeAgo.addDefaultLocale(en)
+const timeAgo = new TimeAgo('en-US');
 const Main = () => {
   const[items, setItems] = useState([
-    {key: 'Feed#1'}, 
-    {key:'Feed#2'}, 
-    {key: 'Feed#3'},
+    {question: 'Feed#1'}, 
+    {question:'Feed#2'}, 
+    {question: 'Feed#3'},
 ]); 
 
   async function addPost(question, time){
@@ -26,19 +29,69 @@ const Main = () => {
   }
   useEffect(()=>{
     const getAll = async ()=>{
-      await getAllRecords();
+      let allData = await getAllRecords();
+      allData.map((post, postIndex)=>{
+        allData[postIndex] = {...allData[postIndex], timePassed: timeAgo.format(allData[postIndex].createAt.toDate())}
+        allData[postIndex].replies.map((reply,replyIndex)=>{
+          allData[postIndex].replies[replyIndex] = {...reply , timePassed: timeAgo.format(allData[postIndex].replies[replyIndex].createAt.toDate())}
+        })
+      })
+      // console.log(timeAgo.format(allData[0].createAt.toDate()))
+      // console.log(allData[0].createAt)
+      setItems(allData)
     }
     getAll()
-  })
+  },[])
+  
+  /*
+  [
+    {
+      postId:
+      question:
+      createAt
+      replies: [
+        {
+          replyId:
+          reply:
+        },
+        {
 
+        }
+      ]
+    },
+    {
+
+    },
+  ]
+  */ 
+
+  // function FlatListCustom({allData}){
+  //   return (
+      
+  //   );
+  // }
+  function DataList({data}){
+   
+    // if (data.length !=0){
+    //   console.log(data[0].createAt.toDate())
+    // }
+    // console.log(data[0].createAt.toDate())
+    return (
+      data.map((item)=>{
+        console.log(item)
+        return <Feed item={item} key={item.postId}/>
+      })
+    );
+  }
   return (
     <View style={styles.container}>
       <Header/>
       <SearchBar/>
-      <FlatList data={items}
+      <DataList data={items}/>
+      {/* <FlatList data={items}
         renderItem = {({item}) => 
-      <Feed item={item} />}
-      />
+      <Feed item={item} time={item.createAt}/>}
+      /> */}
     </View>
   );
 }
