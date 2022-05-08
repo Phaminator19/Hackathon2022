324ef732a3b67b1a63f19bfdd73f1c1a1c3f2a7f
Quang <70238159+Phaminator19@users.noreply.github.com>
2022-05-08 08:23:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ const options = {
     method: 'POST',
     headers: {
       Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: 'Bearer eyJraWQiOiJ4ZFFwRkoxVDRRWnJMWXJYVHR4VVA5SVUxMGh3M240K0FTS1hWcWxaZkNJPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJjMmI4ZjJhOS05N2QyLTQ2YWYtYTlhOC0yOWU2ZGQxZmFhMjgiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9Bd2ZOUzA4bHMiLCJjbGllbnRfaWQiOiI2MXJrM3JhOW9sbjJhMDhlMm9oNG12dnE3ayIsIm9yaWdpbl9qdGkiOiI2NWUxOTY2Yi0yYzk2LTRkN2MtYjIxNi02MzM5ZmQxNTEyYzYiLCJldmVudF9pZCI6ImNhYjU2NmUwLWQ4OGYtNDY3NS04ZmE0LTZjZjU4NzNhYzg1NiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2NTIwMTE0NDgsImV4cCI6MTY1MjA1NDY0NywiaWF0IjoxNjUyMDExNDQ4LCJqdGkiOiIxNjU5YjE5NS05NDIyLTRhMWQtYWM1My0yOGFiYTA2YjdhNzciLCJ1c2VybmFtZSI6IjAycXlneWlpcXh1Y2psamIzYiJ9.GqcViIs8nYdXxbtHeW7BRvu5SGV9VDicu85UcD5l6iukHS7GzK2PjKqbzBCUspFmWbTMQO-AgNVnZ1-y7POOYzEJAiOkEws_Cuwx6D5GFKZGYbsKCwP_lXM0TYY4ywXdFgQwpxEDeNB1V9Cr6Hb2SjVpaGUZDFmfCOqDiFshA4B5KRe5Rym3EbY1FcYxFC6Nvb7DYwLvyMwcdVvWieTm1YKMl5oGf9XrYlnI_d0_flsRv1xb6NagI7ojgJCnZGf-xaO-FPM_Z19F3ta2ICSA7TYufbandDrJBn7tZ7T4FRlBs-h2LpE7BqciNUXN5nMtp3jQkyYcIMv52x-GRfoDWA'
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify({
       companyName: 'ruhacks',
